79e6b198c6f2a70a3c2378205d57bf861b824562
0x0mini <163350822+0x0mini@users.noreply.github.com>
2024-05-02 05:19:53 +0200

    change variable

--- encryption.rs
+++ encryption.rs
@@ -6,48 +6,48 @@ use std::fs::{self, File};
 use std::io::{Write, Error};
 use std::env;
 
-type Aes256CbcEncryption = Cbc<Aes256, Pkcs7>;
+type Aes256Cbc = Cbc<Aes256, Pkcs7>;
 
-fn generate_encryption_key_and_iv() -> (Vec<u8>, Vec<u8>) {
-    let mut encryption_key = vec![0u8; 32];
-    let mut initialization_vector = vec![0u8; 16];
-    OsRng.fill_bytes(&mut encryption_key);
-    OsRng.fill_bytes(&mut initialization_vector);
-    (encryption_key, initialization_vector)
+fn generate_key_and_iv() -> (Vec<u8>, Vec<u8>) {
+    let mut key = vec![0u8; 32];
+    let mut iv = vec![0u8; 16]; // Initialization Vector (IV)
+    OsRng.fill_bytes(&mut key);
+    OsRng.fill_bytes(&mut iv);
+    (key, iv)
 }
 
-pub fn encrypt_file_at_path(file_path: &str, encrypted_file_path: &str) -> Result<(), Error> {
-    let file_contents = fs::read(file_path)?;
-    let (encryption_key, initialization_vector) = generate_encryption_key_and_iv();
+pub fn encrypt_file(input_file_path: &str, output_encrypted_file_path: &str) -> Result<(), Error> {
+    let plain_text = fs::read(input_file_path)?;
+    let (key, iv) = generate_key_and_iv();
 
-    let cipher = match Aes256CbcEncryption::new_from_slices(&encryption_key, &initialization_vector) {
+    let cipher = match Aes256Cbc::new_from_slices(&key, &iv) {
         Ok(c) => c,
         Err(e) => return Err(Error::new(std::io::ErrorKind::Other, e)),
     };
 
-    let encrypted_data = cipher.encrypt_vec(&file_contents);
+    let encrypted_text = cipher.encrypt_vec(&plain_text);
     
-    let mut encrypted_file = File::create(encrypted_file_path)?;
-    encrypted_file.write_all(&initialization_vector)?;
-    encrypted_file.write_all(&encryption_key)?; 
-    encrypted_file.write_all(&encrypted_data)?;
+    let mut encrypted_file = File::create(output_encrypted_file_path)?;
+    encrypted_file.write_all(&iv)?;
+    encrypted_file.write_all(&key)?; 
+    encrypted_file.write_all(&encrypted_text)?;
     Ok(())
 }
 
-pub fn decrypt_file_at_path(encrypted_file_path: &str, decrypted_file_path: &str) -> Result<(), Error> {
-    let encrypted_file_contents = fs::read(encrypted_file_path)?;
-    if encrypted_file_contents.len() < 48 { 
+pub fn decrypt_file(input_encrypted_file_path: &str, output_decrypted_file_path: &str) -> Result<(), Error> {
+    let encrypted_contents = fs::read(input_encrypted_file_path)?;
+    if encrypted_contents.len() < 48 { // Key size (32) + IV size (16)
         return Err(Error::new(std::io::ErrorKind::InvalidInput, "Encrypted file is too short."));
     }
-    let (initialization_vector, rest) = encrypted_file_contents.split_at(16);
-    let (encryption_key, encrypted_data) = rest.split_at(32);
+    let (iv, remaining) = encrypted_contents.split_at(16);
+    let (key, encrypted_text) = remaining.split_at(32);
 
-    let cipher = match Aes256CbcEncryption::new_from_slices(encryption_key, initialization_vector) {
+    let cipher = match Aes256Cbc::new_from_slices(key, iv) {
         Ok(c) => c,
         Err(e) => return Err(Error::new(std::io::ErrorKind::Other, e)),
     };
 
-    let decrypted_data = cipher.decrypt_vec(encrypted_data)?;
-    fs::write(decrypted_file_path, decrypted_data)?;
+    let decrypted_text = cipher.decrypt_vec(encrypted_text)?;
+    fs::write(output_decrypted_file_path, decrypted_text)?;
     Ok(())
 }
