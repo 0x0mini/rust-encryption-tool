bca45fb6a48cc87cf51cb06140bb0f33a3161d29
0x0mini <163350822+0x0mini@users.noreply.github.com>
2024-05-04 02:21:39 +0200

    improve error handling

--- encryption.rs
+++ encryption.rs
@@ -2,10 +2,21 @@ use aes::Aes256;
 use block_modes::{BlockMode, Cbc};
 use block_modes::block_padding::Pkcs7;
 use rand::{rngs::OsRng, RngCore};
-use std::{fs::{self, File}, io::{Write, Error}};
+use std::{fs::{self, File}, io::{Write, Error, ErrorKind}};
 use clap::{App, Arg, SubCommand};
+use thiserror::Error;
 
 type Aes256Cbc = Cbc<Aes256, Pkcs7>;
+
+#[derive(Debug, Error)]
+enum MyError {
+    #[error("Error with the file system: {0}")]
+    FileSystem(#[from] std::io::Error),
+    #[error("Error with crypto operation: {0}")]
+    Crypto(String),
+    #[error("The encrypted file is too short.")]
+    ShortFile,
+}
 
 fn generate_key_and_iv() -> (Vec<u8>, Vec<u8>) {
     let mut key = vec![0u8; 32];
@@ -15,12 +26,13 @@ fn generate_key_and_iv() -> (Vec<u8>, Vec<u8>) {
     (key, iv)
 }
 
-pub fn encrypt_file(input_file_path: &str, output_encrypted_file_path: &str) -> Result<(), Error> {
+pub fn encrypt_file(input_file_path: &str, output_encrypted_file_path: &str) -> Result<(), MyError> {
     let plain_text = fs::read(input_file_path)?;
     let (key, iv) = generate_key_and_iv();
-    let cipher = Aes256Cbc::new_from_slices(&key, &iv).map_err(|e| Error::new(std::io::ErrorKind::Other, e))?;
+    let cipher = Aes256Cbc::new_from_slices(&key, &iv)
+        .map_err(|e| MyError::Crypto(e.to_string()))?;
     let encrypted_text = cipher.encrypt_vec(&plain_text);
-    
+
     let mut encrypted_file = File::create(output_encrypted_file_path)?;
     encrypted_file.write_all(&iv)?;
     encrypted_file.write_all(&key)?;
@@ -28,15 +40,16 @@ pub fn encrypt_file(input_file_path: &str, output_encrypted_file_path: &str) -> 
     Ok(())
 }
 
-pub fn decrypt_file(input_encrypted_file_path: &str, output_decrypted_file_path: &str) -> Result<(), Error> {
+pub fn decrypt_file(input_encrypted_file_path: &str, output_decrypted_file_path: &str) -> Result<(), MyError> {
     let encrypted_contents = fs::read(input_encrypted_file_path)?;
     if encrypted_contents.len() < 48 {
-        return Err(Error::new(std::io::ErrorKind::InvalidInput, "Encrypted file is too short."));
+        return Err(MyError::ShortFile);
     }
     let (iv, remaining) = encrypted_contents.split_at(16);
     let (key, encrypted_text) = remaining.split_at(32);
 
-    let cipher = Aes256Cbc::new_from_slices(key, iv).map_err(|e| Error::new(std::io::ErrorKind::Other, e))?;
+    let cipher = Aes256Cbc::new_from_slices(key, iv)
+        .map_err(|e| MyError::Crypto(e.to_string()))?;
     let decrypted_text = cipher.decrypt_vec(encrypted_text)?;
     fs::write(output_decrypted_file_path, decrypted_text)?;
     Ok(())
