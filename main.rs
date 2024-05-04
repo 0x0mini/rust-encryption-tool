a03cbc02888f5d320dc3628004960dfd935a2c35
0x0mini <163350822+0x0mini@users.noreply.github.com>
2024-05-04 02:41:29 +0200

    clean code

--- main.rs
+++ main.rs
@@ -1,11 +1,24 @@
 use clap::{App, Arg};
 use std::fs;
-use std::io::{self};
+use std::io::{self, Error, ErrorKind};
 
 mod encryption;
 
 fn main() {
-    let matches = App::new("File Encryption Tool")
+    let matches = build_cli().get_matches();
+
+    let operation = matches.value_of("action").expect("Action argument is required");
+    let source_file_path = matches.value_of("source_path").expect("Source file path argument is required");
+    let destination_file_path = matches.value_of("destination_path").expect("Destination file path argument is required");
+    let cipher_key = matches.value_of("encryption_key").expect("Encryption key argument is required").as_bytes();
+
+    if let Err(error) = process_file(operation, source_file_path, destination_file_path, cipher_key) {
+        eprintln!("Error occurred while processing the file: {}", error);
+    }
+}
+
+fn build_cli() -> App<'static> {
+    App::new("File Encryption Tool")
         .version("1.0")
         .author("Your Name. <your_email@example.com>")
         .about("Encrypts or decrypts files")
@@ -34,30 +47,22 @@ fn main() {
                 .required(true)
                 .index(4),
         )
-        .get_matches();
-
-    let operation = matches.value_of("action").expect("Action argument is required");
-    let source_file_path = matches.value_of("source_path").expect("Source file path argument is required");
-    let destination_file_path = matches.value_of("destination_path").expect("Destination file path argument is required");
-    let cipher_key = matches.value_of("encryption_key").expect("Encryption key argument is required").as_bytes();
-
-    if let Err(error) = process_file(operation, source_file_path, destination_file_path, cipher_key) {
-        eprintln!("Error occurred while processing the file: {}", error);
-    }
 }
 
 fn process_file(operation: &str, source_path: &str, destination_path: &str, key: &[u8]) -> io::Result<()> {
     match operation {
-        "encrypt" => encrypt_or_decrypt_file(source_path, destination_path, key).map(|_| println!("Encryption successful.")),
-        "decrypt" => encrypt_or_decrypt_file(source_path, destination_path, key).map(|_| println!("Decryption successful.")),
-        _ => Err(io::Error::new(io::ErrorKind::Other, "Invalid operation. Use 'encrypt' or 'decrypt'.")),
+        "encrypt" => encrypt_or_decrypt_file(source_path, destination_path, key, "Encryption successful."),
+        "decrypt" => encrypt_or_decrypt_file(source_path, destination_path, key, "Decryption successful."),
+        _ => Err(Error::new(ErrorKind::Other, "Invalid operation. Use 'encrypt' or 'decrypt'.")),
     }
 }
 
-fn encrypt_or_decrypt_file(source_file_path: &str, destination_file_path: &str, key: &[u8]) -> io::Result<()> {
+fn encrypt_or_decrypt_file(source_file_path: &str, destination_file_path: &str, key: &[u8], success_message: &str) -> io::Result<()> {
     let content = fs::read(source_file_path)?;
     let processed_content = encryption::xor_operation(&content, key);
-    fs::write(destination_file_path, processed_content)
+    fs::write(destination_file_path, processed_content)?;
+    println!("{}", success_message);
+    Ok(())
 }
 
 mod encryption {
