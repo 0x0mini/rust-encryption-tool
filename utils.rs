e4f52f6d9823add014357563afc2933c11729da3
0x0mini <163350822+0x0mini@users.noreply.github.com>
2024-05-07 22:22:02 +0200

    update with new feature

--- utils.rs
+++ utils.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::fs;
 use std::path::Path;
-use std::io::{self, Write};
+use std::io::{self, Write, Read};
 
 pub fn is_valid_file_path(file_path: &str) -> Result<(), String> {
     let path_obj = Path::new(file_path);
@@ -62,5 +62,25 @@ pub fn log_error_message(message: &str) {
             };
         },
         Err(_) => eprintln!("ERROR: {}", message),
+    }
+}
+
+pub fn encrypt_decrypt_file(file_path: &str, key: u8) -> Result<(), String> {
+    let path = Path::new(file_path);
+
+    if !path.exists() || !path.is_file() {
+        return Err(format!("File does not exist or is not a proper file: {}", file_path));
+    }
+
+    let mut file_content = match fs::read(file_path) {
+        Ok(content) => content,
+        Err(e) => return Err(format!("Failed to read file: {}. Error: {}", file_path, e)),
+    };
+
+    let encrypted_decrypted_data: Vec<u8> = file_content.into_iter().map(|byte| byte ^ key).collect();
+
+    match fs::write(file_path, encrypted_decrypted_data) {
+        Ok(_) => Ok(()),
+        Err(e) => Err(format!("Failed to write back to file: {}. Error: {}", file_path, e)),
     }
 }
